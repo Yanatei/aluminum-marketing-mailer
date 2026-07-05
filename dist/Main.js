7dcdb0b87a30754e06d6ad8ee82bc50785631817
Orc <672497611@qq.com>
2026-07-05 13:51:59 +0800

    a

--- dist/Main.js
+++ dist/Main.js
@@ -1,4 +1,17 @@
 "use strict";
+const SHEET_ID = "17Fsn7FGGHGIwrxea0bOLtmBF5-VMn30nIUxxPu78YvM";
+readSheet();
 function hello() {
     Logger.log("Hello Apps Script");
+}
+function readSheet() {
+    try {
+        const ss = SpreadsheetApp.openById(SHEET_ID);
+        const sheet = ss.getSheets()[0];
+        const data = sheet.getDataRange().getValues();
+        Logger.log(data);
+    }
+    catch (error) {
+        Logger.log(`读取表格失败: ${error}`);
+    }
 }
--- src/Main.ts
+++ src/Main.ts
@@ -1,3 +1,22 @@
+const SHEET_ID = "17Fsn7FGGHGIwrxea0bOLtmBF5-VMn30nIUxxPu78YvM";
+
+readSheet();
+
 function hello() {
   Logger.log("Hello Apps Script");
+}
+
+function readSheet(): void {
+  try {
+    const ss = SpreadsheetApp.openById(SHEET_ID);
+
+    const sheet = ss.getSheets()[0];
+
+    const data = sheet.getDataRange().getValues();
+
+    Logger.log(data);
+
+  } catch (error) {
+    Logger.log(`读取表格失败: ${error}`);
+  }
 }
